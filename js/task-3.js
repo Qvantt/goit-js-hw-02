dd19e00f7e0bca2d7fe8755c02b3a1e628c0306f
Qvantt <65224747+Qvantt@users.noreply.github.com>
2024-05-16 13:17:31 +0300

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,18 @@
-function filterArray(numbers, value) {
-  let arrayX = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      arrayX.push(numbers[i]);
-    }
-  }
-  return arrayX;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(newTime) {
+    this.playTime += newTime;
+  },
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
